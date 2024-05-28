0a59e84fd3000782a8149547b84b901e18533655
Mikhail Vysogorets <m.vysogorets@wwpass.com>
2024-05-28 10:40:47 -0400

    src dir

---
+++ src/main/java/com/wwpass/keycloak/connection/WWPassConnectionFactory.java
@@ -0,0 +1,6 @@
+package com.wwpass.keycloak.connection;
+
+import org.keycloak.provider.ProviderFactory;
+
+public interface WWPassConnectionFactory extends ProviderFactory<WWPassConnectionProvider> {
+}
